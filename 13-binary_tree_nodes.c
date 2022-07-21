77969a6d29f4b8030527ae920a91e491d8176f05
Achref-haddaji <haddaji.achref19@gmail.com>
2022-07-21 09:12:17 +0100

    task13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,33 +1,35 @@
 #include "binary_trees.h"
+
 /**
- * binary_tree_nodes - Entry point
- *
- * @tree: pointer to the root node
- * Return:  0
- */
-size_t binary_tree_nodes(const binary_tree_t *tree){
-//size_t heightLeft = 0, heightRight = 0;
-
-//if (tree == NULL)
-//return (0);
-//heightLeft = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-//heightRight = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-
-//if (heightLeft > heightRight)
-//return (heightLeft);
-//else
-//return (heightRight);
-return (binary_tree_height(tree->left) + binary_tree_height(tree->right));
-}
-size_t binary_tree_height(const binary_tree_t *tree)
+* binary_tree_nodes -  a function that counts the nodes with
+* at least 1 child in a binary tree
+* @tree: a pointer to the root tree of the tree to traverse
+* Return: the nodes with at least 1 child in a binary tree
+*/
+size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-size_t heightLeft = 1, heightRight = 0;
+	if (tree == NULL || binary_tree_is_leaf(tree))
+		return (0);
+	if (tree->left == NULL && tree->right != NULL)
+		return (1 + binary_tree_nodes(tree->right));
+	else if (tree->left != NULL && tree->right == NULL)
+		return (1 + binary_tree_nodes(tree->left));
+	else
+		return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right) + 1);
+}
 
-if (tree == NULL)
-return (0);
-heightLeft = tree->left ? 1 + binary_tree_height(tree->left) : 1;
-heightRight = tree->right ? 1 + binary_tree_height(tree->right) : 1;
+#include "binary_trees.h"
+/**
+ * binary_tree_is_leaf - checks if a node is a leaf
+ * @node: a pointer to the node to check
+ * Return: 1 if node is a leaf, otherwise 0
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
 
-return(heightLeft);
-return(heightRight);
+	if (node->right == NULL && node->left == NULL)
+		return (1);
+	return (0);
 }
